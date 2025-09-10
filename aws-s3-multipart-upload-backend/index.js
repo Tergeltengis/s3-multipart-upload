481acc1360d849ea920600c33c035b11e518ca7d
tergeltengis <tergeltengis77@gmail.com>
2025-09-10 14:03:11 +0800

    initial commit

---
+++ aws-s3-multipart-upload-backend/index.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const AWS = require("aws-sdk");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// get all the environment variable from .env file
+require("dotenv").config();
+
+// Create express app
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+// add body parser middleware for json
+app.use(bodyParser.json());
+
+// AWS S3 config
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  signatureVersion: "v4",
+});
+
+// endpoint to check if API is live
+app.get("/", (req, res) => {
+  return res.status(200).send("API is live! 🎉😎");
+});
+
+// Generate single presigned url to upload file
+app.post("/generate-single-presigned-url", async (req, res) => {
+  try {
+    const fileName = req.body.fileName;
+
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: fileName,
+      Expires: 60, // Expires in 60 seconds
+      ACL: "bucket-owner-full-control",
+    };
+
+    let url = await s3.getSignedUrlPromise("putObject", params);
+
+    return res.status(200).json({ url });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Error generating presigned URL" });
+  }
+});
+
+// endpoint to start multipart upload
+app.post("/start-multipart-upload", async (req, res) => {
+  // initialization
+  let fileName = req.body.fileName;
+  let contentType = req.body.contentType;
+
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: fileName,
+  };
+
+  // add extra params if content type is video
+  if (contentType == "VIDEO") {
+    params.ContentDisposition = "inline";
+    params.ContentType = "video/mp4";
+  }
+
+  try {
+    const multipart = await s3.createMultipartUpload(params).promise();
+    res.json({ uploadId: multipart.UploadId });
+  } catch (error) {
+    console.error("Error starting multipart upload:", error);
+    return res.status(500).json({ error: "Error starting multipart upload" });
+  }
+});
+
+// Generate presigned url for each multiparts
+app.post("/generate-presigned-url", async (req, res) => {
+  // get values from req body
+  const { fileName, uploadId, partNumbers } = req.body;
+  const totalParts = Array.from({ length: partNumbers }, (_, i) => i + 1);
+  try {
+    const presignedUrls = await Promise.all(
+      totalParts.map(async (partNumber) => {
+        const params = {
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          Key: fileName,
+          PartNumber: partNumber,
+          UploadId: uploadId,
+          Expires: 3600 * 3,
+        };
+
+        return s3.getSignedUrl("uploadPart", {
+          ...params,
+        });
+      })
+    );
+    res.json({ presignedUrls });
+  } catch (error) {
+    console.error("Error generating pre-signed URLs:", error);
+    return res.status(500).json({ error: "Error generating pre-signed URLs" });
+  }
+});
+
+// Complete multipart upload
+app.post("/complete-multipart-upload", async (req, res) => {
+  // Req body
+  let fileName = req.body.fileName;
+  let uploadId = req.body.uploadId;
+  let parts = req.body.parts;
+  console.log(req.body);
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: fileName,
+    UploadId: uploadId,
+
+    MultipartUpload: {
+      Parts: parts.map((part, index) => ({
+        ETag: part.etag,
+        PartNumber: part.PartNumber,
+      })),
+    },
+  };
+  try {
+    const data = await s3.completeMultipartUpload(params).promise();
+    res.status(200).json({ fileData: data });
+  } catch (error) {
+    console.error("Error completing multipart upload:", error);
+    return res.status(500).json({ error: "Error completing multipart upload" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} - http://localhost:${PORT}`);
+});
